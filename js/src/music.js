d3740685659fda30accaed44edff5ed9d78be979
EvilLeaker <evilleaker@outlook.com>
2023-11-11 02:08:55 +0800

    Fixed 3

--- js/src/music.js
+++ js/src/music.js
@@ -41,7 +41,7 @@ var make_music_item = function (
     thumb.classList.add("new");
   }
   var thumb_img = document.createElement("img");
-  thumb_img.src = "https://chunithm-net-eng.com/mobile/img/" + image;
+  thumb_img.src = "img/music/jacket" + image;
   thumb.appendChild(thumb_img);
   wrapper.appendChild(thumb);
 
